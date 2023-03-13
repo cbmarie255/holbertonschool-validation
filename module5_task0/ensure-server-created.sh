c1d9c5312436d3ba3a96eec0717b170ab106123b
cbmarie255 <court.b1993@gmail.com>
2023-03-13 13:40:23 -0500

    AWS account changes

--- module5_task0/ensure-server-created.sh
+++ module5_task0/ensure-server-created.sh
@@ -4,8 +4,8 @@
 aws ec2 run-instances \
 	--image-id ami-0568936c8d2b91c4e \
 	--instance-type t2.micro \
-	--subnet-id subnet-0278268b3b9b7c4a4 \
-	--security-group-ids sg-0409af206a041b700 \
+	--subnet-id subnet-013affb718a597ad0 \
+	--security-group-ids sg-01e234840d346627e \
 	--key-name awesome-key \
 	--query 'Instances[*].InstanceId' \
 	--output text \
