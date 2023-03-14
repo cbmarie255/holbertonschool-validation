71e02a9d0588562930d374d0b5ea8076a3938a64
cbmarie255 <court.b1993@gmail.com>
2023-03-14 11:32:02 -0500

    Deploy shellcheck

--- module5_task2/deploy.sh
+++ module5_task2/deploy.sh
@@ -6,7 +6,7 @@ new_ec2_instance_public_ip=$(cat new_ec2_instance_ip.txt)
 # Read the name of the tag to use with the docker image
 dockerImageTag=$(cat awesome_image_tag_name.txt)
 
-docker context create production --docker "host=ssh://ubuntu@new_ec2_instance_public_ip"
+docker context create production --docker "host=ssh://ubuntu@$new_ec2_instance_public_ip"
 docker context use production
 
 docker pull cbmarie255/awesome-web:"$dockerImageTag"
