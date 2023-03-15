8c7417385df7428b01b86b952438c3d9b9135e62
cbmarie255 <court.b1993@gmail.com>
2023-03-15 14:25:57 -0500

    more pratice bruh

--- module5_task2/ensure-server-created.sh
+++ module5_task2/ensure-server-created.sh
@@ -2,10 +2,10 @@
 
 # Create the EC2 instance and print the public IP address
 aws ec2 run-instances \
-	--image-id ami-0568936c8d2b91c4e \
+	--image-id ami-09cd747c78a9add63 \
 	--instance-type t2.micro \
-	--subnet-id subnet-013affb718a597ad0 \
-	--security-group-ids sg-01e234840d346627e \
+	--subnet-id subnet-0bae4711d45d53358 \
+	--security-group-ids sg-04100bb59c5a791bd \
 	--key-name awesome-key \
 	--query 'Instances[*].InstanceId' \
 	--output text \
