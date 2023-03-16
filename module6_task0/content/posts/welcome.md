42986f2f449876eda689da26dfcfde763e47d6d2
cbmarie255 <court.b1993@gmail.com>
2023-03-16 15:03:59 -0500

    adding initial files

--- module6_task0/content/posts/welcome.md
+++
@@ -1,6 +0,0 @@
----
-title: "Welcome to Awesome Inc."
-date: 2023-01-11T14:02:41-06:00
-draft: true
----
-
--- module6_task0/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt update
-apt upgrade -y
-apt install -y wget make shellcheck zip
-
-# Download the Hugo executable
-wget https://github.com/gohugoio/hugo/releases/download/v0.110.0/hugo_0.110.0_Linux-64bit.tar.gz
-tar -C /usr/local/bin -xzf hugo_0.110.0_Linux-64bit.tar.gz
-rm ./hugo_0.110.0_Linux-64bit.tar.gz
